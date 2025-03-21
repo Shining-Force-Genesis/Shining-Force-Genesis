7639d7dcc97513d45c8cc31883f6f0a37cabd30b
Mak <mar.music.vault@gmail.com>
2025-03-21 12:32:06 -0400

    Add chapter 3 maps layout

---
+++ SF-Maps/SF1/Chapter-3/Overworld/C2-Overworld-Ref.tsx
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="C2-Overworld-Ref" tilewidth="2616" tileheight="984" tilecount="4" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="768" height="888" source="../../Maps_Ref/ShiningForce-LegacyOfGreatIntention-Chapter2-SpiritOfTheHolySpring.png"/>
+ </tile>
+ <tile id="2">
+  <image width="768" height="888" source="../../Maps_Ref/ShiningForce-LegacyOfGreatIntention-Chapter3-SecretWeaponOfRunefaust.png"/>
+ </tile>
+ <tile id="3">
+  <image width="1344" height="480" source="../../Maps_Ref/ShiningForce-LegacyOfGreatIntention-Chapter3-SecretWeaponOfRunefaust-Bridge.png"/>
+ </tile>
+ <tile id="4">
+  <image width="2616" height="984" source="../../Maps_Ref/ShiningForce-LegacyOfGreatIntention-Chapter3-SecretWeaponOfRunefaust-Bustoke.png"/>
+ </tile>
+</tileset>
